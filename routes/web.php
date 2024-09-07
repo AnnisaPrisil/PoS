b51cf0acdac9dfacc084a85c9bf4a2cd4db7e022
Annisa P <annisaprissilya@gmail.com>
2024-09-07 12:07:35 +0700

    JS 2 PWL

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SalesController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +16,12 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::prefix('category')->group(function () {
+    Route::get('food-beverage', [ProductController::class, 'foodBeverage'])->name('products.food-beverage');
+    Route::get('beauty-health', [ProductController::class, 'beautyHealth'])->name('products.beauty-health');
+    Route::get('home-care', [ProductController::class, 'homeCare'])->name('products.home-care');
+    Route::get('baby-kid', [ProductController::class, 'babyKid'])->name('products.baby-kid');
 });
+Route::get('/user/{id}/name/{name}', [UserController::class, 'profile'])->name('user.profile');
+Route::get('/sales', [SalesController::class, 'index'])->name('sales.index');
